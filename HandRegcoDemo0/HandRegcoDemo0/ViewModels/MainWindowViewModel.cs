161c641d7c0a5fa7fe458de17196d57da46864f5
Bumchic <duonganhminh05@gmail.com>
2025-04-20 23:22:43 +0700

    Add Camera Selector

--- HandRegcoDemo0/HandRegcoDemo0/ViewModels/MainWindowViewModel.cs
+++ HandRegcoDemo0/HandRegcoDemo0/ViewModels/MainWindowViewModel.cs
@@ -2,8 +2,10 @@
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Windows.Devices.Enumeration;
-using Windows.Media.Capture;
+
 using Avalonia.Threading;
+using System.Collections.Generic;
+
 
 
 
@@ -12,20 +14,6 @@ namespace HandRegcoDemo0.ViewModels
     public partial class MainWindowViewModel : ViewModelBase
     {
         public string Greeting { get; set; }
-        public DeviceInformationCollection devices { get; set; }
-        public MainWindowViewModel()
-        {
-            AsyncFindAllDevice();
-        }
 
-
-        public async Task AsyncFindAllDevice()
-        {
-             DeviceInformationCollection collection = await DeviceInformation.FindAllAsync();
-            foreach(DeviceInformation device in collection)
-            {
-                Debug.WriteLine(device.GetType());
-            }
-        }
     }
 }
--- HandRegcoDemo0/HandRegcoDemo0/Views/MainWindow.axaml.cs
+++ HandRegcoDemo0/HandRegcoDemo0/Views/MainWindow.axaml.cs
@@ -1,11 +1,39 @@
 using Avalonia.Controls;
+using Windows.Devices.Enumeration;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.Media;
+using Windows.Media.Capture;
+using Windows.Media.Devices;
+
 
 namespace HandRegcoDemo0.Views;
 
 public partial class MainWindow : Window
 {
+    public VideoDeviceController media { get; set; }
+    public MediaCapture mediaCapture { get; set; }
     public MainWindow()
     {
         InitializeComponent();
+        InitCapMedia();
+        AddCameraOption();
+        
     }
+    public async Task InitCapMedia()
+    {
+        mediaCapture = new MediaCapture();
+        await mediaCapture.InitializeAsync();
+    }
+    public async Task AddCameraOption()
+    {
+        DeviceInformationCollection devices = await DeviceInformation.FindAllAsync(MediaDevice.GetVideoCaptureSelector());
+        Debug.WriteLine(devices.Count);
+        foreach(DeviceInformation item in devices)
+        {
+            DeviceChoices.Items.Add(item.Name);
+        }
+    }
+
 }
