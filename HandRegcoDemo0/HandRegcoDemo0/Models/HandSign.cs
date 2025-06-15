7d0c2198022ab72332e1e181ddb12e50ec75f224
Bumchic <duonganhminh05@gmail.com>
2025-06-15 11:37:57 +0700

    Create HandSign.cs

---
+++ HandRegcoDemo0/HandRegcoDemo0/Models/HandSign.cs
@@ -0,0 +1,16 @@
+﻿using Emgu.CV.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandRegcoDemo0.Models
+{
+    class HandSign
+    {
+        public string Word { get; set; }
+        public VectorOfPoint contour { get; set; }
+        public VectorOfPoint convexHull { get; set; }
+    }
+}
