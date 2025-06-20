2b638401986aff5b5fb890d4d586671c54c10bc7
DoThanhLoc2334 <locdothanh233@gmail.com>
2025-06-20 22:06:38 +0700

    change load file way

--- HandRegcoDemo0/HandRegcoDemo0/Models/HandSign.cs
+++ HandRegcoDemo0/HandRegcoDemo0/Models/HandSign.cs
@@ -1,5 +1,4 @@
-﻿using DynamicData;
-using Emgu.CV;
+﻿using Emgu.CV;
 using Emgu.CV.Structure;
 using Emgu.CV.Util;
 using HandRegcoDemo0.ViewModels;
@@ -7,9 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HandRegcoDemo0.Models
 {
@@ -20,61 +16,64 @@ namespace HandRegcoDemo0.Models
         public VectorOfPoint convexHull { get; set; }
         public Rectangle box { get; set; }
         public double distanceFromFirstCorner { get; set; }
+
+        public HandSign() 
+        {
+            this.Word = "Empty";
+            this.contour = new VectorOfPoint();
+            this.convexHull = new VectorOfPoint();
+        }
+
         public HandSign(Mat img, string word)
         {
             ImageProcesser _imageProcessor = new ImageProcesser();
+
             CvInvoke.CvtColor(img, img, Emgu.CV.CvEnum.ColorConversion.Bgr2Bgra);
             img = _imageProcessor.DetectSkinVer1(img);
+
             VectorOfPoint contour = _imageProcessor.FindLargestContour(img);
             VectorOfPoint hull = _imageProcessor.GetConvexHull(contour);
             Rectangle box = _imageProcessor.getBoundingBox(contour);
             double distFromCorner = new DistanceArithmetic().DistanceFromBoxFirstCorner(hull, box);
+
             this.contour = contour;
             this.convexHull = hull;
             this.box = box;
             this.distanceFromFirstCorner = distFromCorner;
             this.Word = word;
         }
-        public HandSign()
-        {
-            this.Word = "Empty";
-            this.contour = new VectorOfPoint();
-            this.convexHull = new VectorOfPoint();
-        }
-        public HandSign(Mat img)
-        {
-            ImageProcesser _imageProcessor = new ImageProcesser();
-            CvInvoke.CvtColor(img, img, Emgu.CV.CvEnum.ColorConversion.Bgr2Bgra);
-            img = _imageProcessor.DetectSkinVer1(img);
-            VectorOfPoint contour = _imageProcessor.FindLargestContour(img);
-            VectorOfPoint hull = _imageProcessor.GetConvexHull(contour);
-            Rectangle box = _imageProcessor.getBoundingBox(contour);
-            double distFromCorner = new DistanceArithmetic().DistanceFromBoxFirstCorner(hull, box);
-            this.contour = contour;
-            this.convexHull = hull;
-            this.box = box;
-            this.distanceFromFirstCorner = distFromCorner;
-            this.Word = "This is Input for Mat";
-        }
+
+        public HandSign(Mat img) : this(img, "This is Input for Mat") { }
+
         public List<HandSign> PopulateHandSign()
         {
             DirectoryInfo directory = new DirectoryInfo("Assets");
-            FileInfo[] infos = directory.GetFiles();
-            List<HandSign> imagelist = new List<HandSign>();
-            for (int i = 0; i < infos.Length; i++)
-            {
 
-                if (infos[i].Extension == ".jpg")
-                {   
-                    byte[] bytes = File.ReadAllBytes(infos[i].FullName);
+            FileInfo[] infos = directory.GetFiles("*.jpg");
+            List<HandSign> imagelist = new List<HandSign>();
+
+            foreach (var file in infos)
+            {
+                try
+                {
+                    byte[] bytes = File.ReadAllBytes(file.FullName);
                     Mat img = new Mat();
                     CvInvoke.Imdecode(bytes, Emgu.CV.CvEnum.ImreadModes.Unchanged, img);
-                    HandSign handSign = new HandSign(img, infos[i].Name.Substring(infos[i].Name.Length - 5, 1));
+
+                    string label = Path.GetFileNameWithoutExtension(file.Name);
+
+                    HandSign handSign = new HandSign(img, label);
                     imagelist.Add(handSign);
+
+                    Console.WriteLine($"Loaded {file.Name} → Label: {label}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Failed to load {file.Name}: {ex.Message}");
                 }
             }
+
             return imagelist;
         }
-        
     }
 }
